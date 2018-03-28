136352341af60c6eb50be3880e13fb1cd4ff988a
Kris Shaffer <kris.shaffer@gmail.com>
2018-03-28 10:01:03 -0400

    cleanup

--- index.php
+++ index.php
@@ -5,8 +5,14 @@
     <title>Bootstrap Starter Kit | Kris Shaffer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link rel="stylesheet" href="/css/bootstrap.css" media="screen">
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/custom.css">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Merriweather:300,700,700italic,300italic|Open+Sans:700,400" />
+
+    <!-- Font Awesome for awesome icons. You can redefine icons used in a plugin configuration -->
+    <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="../bower_components/html5shiv/dist/html5shiv.js"></script>
@@ -20,6 +26,8 @@
       include('./php_includes/navbar.html');
     ?>
 
+    <div id="banner-image"><img src="/files/banner-everest.jpg" alt="Everest" style="width: 100%; padding: 0px; margin: 0px 0px -40px 0px; border: 0px;" /></div>
+
     <div class="container">
 
       <div class="page-header" id="banner">
@@ -31,28 +39,31 @@
         <div class="row">
           <div class="col-lg-9 col-md-8 col-sm-7">
             <p class="lead">Get started on a static HTML site with an out-of-the-box <a href="http://getbootstrap.com/" target="blank_">Bootstrap</a> build.</p>
-            <p>Building your first website with HTML and CSS is usually a lot harder than it needs to be. Either the site is ugly, or the process of making it both functional and attractive is onerous -- both of which can be frustrating for a new developer.</p>
+            <p>Building your first website with HTML and CSS is usually a lot harder than it needs to be. Either the site is ugly, or the process of making it both functional and attractive is onerous — both of which can be frustrating for a new developer.</p>
             <p>Bootstrap is an HTML, CSS, and JavaScript framework that promises to make it much easier to make an attractive, functional site from scratch. But I've found it difficult to collect all the right frameworks and files together, especially if you want a custom theme.</p>
             <p>In the process of helping one of my students get started with her own Bootstrap-enabled site, I collected all the files needed to get started in one ZIP file. Simply download this zip file, upload it to your web host's file manager, extract it, and start editing! No tracking down dependencies, no changing links to different folders... It's all here.</p>
-            <p>You'll find two versions of Bootstrap Starter Kit. The first version is built from HTML, CSS, and a little JavaScript. This can run pretty much anywhere (including GitHub Pages), but it does require you to rewrite the HTML code for the header/footer on every page. Add a link to the navbar? You have to add it everywhere. For small sites, that may be fine, but it can get old.</p>
-            <p>So I also made a version that uses a little PHP to pull the header and footer for every page from a single file. It diminishes the amount of work you have to do, especially for sites with more than two or three pages. It should work on any shared hosting environment (including Domain of One's Own). Just note that it won't work on GitHub Pages.</p>
-            <p>Check out the <a href="/examples.php">examples page</a> to get some ideas of what this theme can do. Have fun!</p>
+            <p>While putting this together, I added a little PHP to pull the header and footer for every page from a single file. It diminishes the amount of work you have to do, especially for sites with more than two or three pages. It should work on any shared hosting environment (including Domain of One's Own).</p>
+            <p>Check out the <a href="/examples.php">examples page</a> to get some ideas of what this theme can do. And for a WYSIWYG (what-you-see-is-what-you-get) text editor that will output HTML code for your Bootstrap site, check out <a href="https://peasycomposer.pushpullfork.com" target="blank_">Peasy Composer</a>.</p>
+            <p>Have fun!</p>
           </div>
 
           <div class="col-lg-3 col-lg-4 col-lg-5">
             <h2 style="margin-top: 0px; text-align:center;">Download BSK</h2>
             <hr/>
             <p class="bs-component" style="text-align: center;">
-              <a href="/bootstrap_starter_kit_html.zip" class="btn btn-default btn-lg">HTML/CSS</a>
+              <a href="/bootstrap_starter_kit_default.zip" class="btn btn-default btn-lg">Starter theme</a>
             </p>
             <p class="bs-component" style="text-align: center;">
-              <a href="bootstrap_starter_kit_php.zip" class="btn btn-default btn-lg">HTML/CSS/PHP</a>
+              <a href="bootstrap_starter_kit_unity.zip" class="btn btn-default btn-lg">Unity theme</a>
             </p>
+            <hr/>
+            <p><small>This site is built with the Starter theme. To see Unity in action, check out my <a href="https://data101.pushpullfork.com" target="blank_">DATA 101 course website</a>.</small></p>
           </div>
         </div>
 
 
         <!-- footer -->
+        <hr/>
         <?php
           include('./php_includes/footer.html');
         ?>
---
+++ longtext.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap Starter Kit | Kris Shaffer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/custom.css">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Merriweather:300,700,700italic,300italic|Open+Sans:700,400" />
+
+    <!-- Font Awesome for awesome icons. You can redefine icons used in a plugin configuration -->
+    <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../bower_components/html5shiv/dist/html5shiv.js"></script>
+      <script src="../bower_components/respond/dest/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <!-- navbar -->
+    <?php
+      include('./php_includes/navbar.html');
+    ?>
+
+    <div class="container">
+
+      <div class="page-header" id="banner">
+        <div class="row">
+          <div class="col-lg-2"></div>
+          <div class="col-lg-8">
+            <h1>Anna Karenina</h1>
+            <h2>Leo Tolstoy</h2>
+            <h3>Translated by Constance Garnett</h3>
+            <hr/>
+            <p><i>This page is a sample long-text page. Rather than a full-width page, it makes use of Bootstrap's column layout to create a responsive text block with a more manageable width for reading.</i></p>
+            <hr/>
+            <p style="text-align: center;">PART ONE</p>
+            <p style="text-align: center;">Chapter 1</p>
+            <p>Happy families are all alike; every unhappy family is unhappy in
+            its own way.</p>
+            <p>Everything was in confusion in the Oblonskys’ house. The wife
+            had discovered that the husband was carrying on an intrigue with
+            a French girl, who had been a governess in their family, and she
+            had announced to her husband that she could not go on living in
+            the same house with him. This position of affairs had now lasted
+            three days, and not only the husband and wife themselves, but all
+            the members of their family and household, were painfully
+            conscious of it. Every person in the house felt that there was
+            no sense in their living together, and that the stray people
+            brought together by chance in any inn had more in common with one
+            another than they, the members of the family and household of the
+            Oblonskys. The wife did not leave her own room, the husband had
+            not been at home for three days. The children ran wild all over
+            the house; the English governess quarreled with the housekeeper,
+            and wrote to a friend asking her to look out for a new situation
+            for her; the man-cook had walked off the day before just at
+            dinner time; the kitchen-maid, and the coachman had given
+            warning.</p>
+            <p>Three days after the quarrel, Prince Stepan Arkadyevitch
+            Oblonsky—Stiva, as he was called in the fashionable world—woke
+            up at his usual hour, that is, at eight o’clock in the morning,
+            not in his wife’s bedroom, but on the leather-covered sofa
+            in his study. He turned over his stout, well-cared-for person
+            on the springy sofa, as though he would sink into a long
+            sleep again; he vigorously embraced the pillow on the other side
+            and buried his face in it; but all at once he jumped up, sat up
+            on the sofa, and opened his eyes.</p>
+            <p>"Yes, yes, how was it now?" he thought, going over his dream.
+            "Now, how was it? To be sure! Alabin was giving a dinner at
+            Darmstadt; no, not Darmstadt, but something American. Yes, but
+            then, Darmstadt was in America. Yes, Alabin was giving a dinner
+            on glass tables, and the tables sang, <i>Il mio tesoro</i>—not <i>Il mio
+            tesoro</i> though, but something better, and there were some sort of
+            little decanters on the table, and they were women, too," he
+            remembered.</p>
+            <p>Stepan Arkadyevitch’s eyes twinkled gaily, and he pondered with a
+            smile. "Yes, it was nice, very nice. There was a great deal
+            more that was delightful, only there’s no putting it into words,
+            or even expressing it in one’s thoughts awake." And noticing a
+            gleam of light peeping in beside one of the serge curtains, he
+            cheerfully dropped his feet over the edge of the sofa, and felt
+            about with them for his slippers, a present on his last birthday,
+            worked for him by his wife on gold-colored morocco. And, as he
+            had done every day for the last nine years, he stretched out his
+            hand, without getting up, towards the place where his
+            dressing-gown always hung in his bedroom. And thereupon he
+            suddenly remembered that he was not sleeping in his wife’s room,
+            but in his study, and why: the smile vanished from his face, he
+            knitted his brows.</p>
+            <p>"Ah, ah, ah! Oo!..." he muttered, recalling everything that had
+            happened. And again every detail of his quarrel with his wife
+            was present to his imagination, all the hopelessness of his
+            position, and worst of all, his own fault.</p>
+            <p>"Yes, she won’t forgive me, and she can’t forgive me. And the
+            most awful thing about it is that it’s all my fault—all my
+            fault, though I’m not to blame. That’s the point of the whole
+            situation," he reflected. "Oh, oh, oh!" he kept repeating in
+            despair, as he remembered the acutely painful sensations caused
+            him by this quarrel.</p>
+            <p>Most unpleasant of all was the first minute when, on coming,
+            happy and good-humored, from the theater, with a huge pear in his
+            hand for his wife, he had not found his wife in the drawing-room,
+            to his surprise had not found her in the study either, and saw
+            her at last in her bedroom with the unlucky letter that revealed
+            everything in her hand.</p>
+            <p>She, his Dolly, forever fussing and worrying over household
+            details, and limited in her ideas, as he considered, was sitting
+            perfectly still with the letter in her hand, looking at him with
+            an expression of horror, despair, and indignation.</p>
+            <p>"What’s this? this?" she asked, pointing to the letter.</p>
+            <p>And at this recollection, Stepan Arkadyevitch, as is so often the
+            case, was not so much annoyed at the fact itself as at the way in
+            which he had met his wife’s words.</p>
+            <p>There happened to him at that instant what does happen to people
+            when they are unexpectedly caught in something very disgraceful.
+            He did not succeed in adapting his face to the position in which
+            he was placed towards his wife by the discovery of his fault.
+            Instead of being hurt, denying, defending himself, begging
+            forgiveness, instead of remaining indifferent even—anything
+            would have been better than what he did do—his face utterly
+            involuntarily (reflex spinal action, reflected Stepan
+            Arkadyevitch, who was fond of physiology)—utterly involuntarily
+            assumed its habitual, good-humored, and therefore idiotic smile.</p>
+            <p>This idiotic smile he could not forgive himself. Catching sight
+            of that smile, Dolly shuddered as though at physical pain, broke
+            out with her characteristic heat into a flood of cruel words, and
+            rushed out of the room. Since then she had refused to see her
+            husband.</p>
+            <p>"It’s that idiotic smile that’s to blame for it all," thought
+            Stepan Arkadyevitch.</p>
+            <p>"But what’s to be done? What’s to be done?" he said to himself
+            in despair, and found no answer.</p>
+
+
+            <!-- footer -->
+            <hr/>
+            <?php
+              include('./php_includes/footer.html');
+            ?>
+          </div>
+          <div class="col-lg-2"></div>
+        </div>
+    </div>
+
+
+    <script style="display: none !important;">!function(e,t,r,n,c,a,l){function i(t,r){return r=e.createElement('div'),r.innerHTML='<a href="'+t.replace(/"/g,'&quot;')+'"></a>',r.childNodes[0].getAttribute('href')}function o(e,t,r,n){for(r='',n='0x'+e.substr(t,2)|0,t+=2;t<e.length;t+=2)r+=String.fromCharCode('0x'+e.substr(t,2)^n);return i(r)}try{for(c=e.getElementsByTagName('a'),l='/cdn-cgi/l/email-protection#',n=0;n<c.length;n++)try{(t=(a=c[n]).href.indexOf(l))>-1&&(a.href='mailto:'+o(a.href,t+l.length))}catch(e){}for(c=e.querySelectorAll('.__cf_email__'),n=0;n<c.length;n++)try{(a=c[n]).parentNode.replaceChild(e.createTextNode(o(a.getAttribute('data-cfemail'),0)),a)}catch(e){}}catch(e){}}(document);</script><script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/custom.js"></script>
+  </body>
+</html>
